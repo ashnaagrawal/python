4d333a40e22b3156d6f36d52924540280ba3df15
ashnaagrawal <51378366+ashnaagrawal@users.noreply.github.com>
2019-06-17 04:07:12 -0700

    Add files via upload

---
+++ whatsapp.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+import webbrowser
+
+webbrowser.open_new_tab('https://api.whatsapp.com/send?phone=whatsappnumber&text=Hey%20this%20message%20is%20sent%20using%20python3')
+
+
+# %20 is used to give space between two words
+''' phone number in phone=912365147896 where 91 is the country code and after that the
+     whatsapp number begins '''
