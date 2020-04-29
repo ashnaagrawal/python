7466de6fe755fec32aeaddec91d1e7eb97d17401
ashnaagrawal <51378366+ashnaagrawal@users.noreply.github.com>
2020-04-29 21:52:31 +0530

    Add files via upload

---
+++ avg.py
@@ -0,0 +1,5 @@
+print('THis program calculates the avg of two numbers.');
+num1 = 4;
+num2 = 8;
+print('the average of', num1, ' and' , num2);
+print('The average is:', (num1 + num2)/2 );
---
+++ avg1.py
@@ -0,0 +1,7 @@
+print("This program calculates the avg of two numbers");
+
+num1 = float(input('enter the first number:'));
+num2 =  float(input('enter the second number:'));
+
+print('the average of', num1 ,'&',num2 ,'is :', (num1+num2)/2);
+             
